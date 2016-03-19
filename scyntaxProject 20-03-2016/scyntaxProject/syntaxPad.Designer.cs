d3fab115e57c5af79891779ed4dee70e0fc02198
AsadMarfani <asad_marfani@outlook.com>
2016-03-20 03:04:06 +0500

    All constants done but there is a white space problem in the RE of String Constants

---
+++ scyntaxProject 20-03-2016/scyntaxProject/syntaxPad.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace scyntaxProject
+{
+    partial class syntaxPad
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CompileButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TokenTextBox = new System.Windows.Forms.TextBox();
+            this.CodeTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // CompileButton
+            // 
+            this.CompileButton.Location = new System.Drawing.Point(210, 483);
+            this.CompileButton.Name = "CompileButton";
+            this.CompileButton.Size = new System.Drawing.Size(75, 23);
+            this.CompileButton.TabIndex = 8;
+            this.CompileButton.Text = "Compile";
+            this.CompileButton.UseVisualStyleBackColor = true;
+            this.CompileButton.Click += new System.EventHandler(this.CompileButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(658, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Token Viewer";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(207, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "CodePad";
+            // 
+            // TokenTextBox
+            // 
+            this.TokenTextBox.Location = new System.Drawing.Point(555, 47);
+            this.TokenTextBox.Multiline = true;
+            this.TokenTextBox.Name = "TokenTextBox";
+            this.TokenTextBox.ReadOnly = true;
+            this.TokenTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TokenTextBox.Size = new System.Drawing.Size(279, 411);
+            this.TokenTextBox.TabIndex = 5;
+            // 
+            // CodeTextBox
+            // 
+            this.CodeTextBox.Location = new System.Drawing.Point(12, 47);
+            this.CodeTextBox.Multiline = true;
+            this.CodeTextBox.Name = "CodeTextBox";
+            this.CodeTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.CodeTextBox.Size = new System.Drawing.Size(536, 412);
+            this.CodeTextBox.TabIndex = 4;
+            this.CodeTextBox.WordWrap = false;
+            // 
+            // syntaxPad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(846, 535);
+            this.Controls.Add(this.CompileButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TokenTextBox);
+            this.Controls.Add(this.CodeTextBox);
+            this.Name = "syntaxPad";
+            this.Text = "syntaxPad";
+            this.Load += new System.EventHandler(this.syntaxPad_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CompileButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TokenTextBox;
+        private System.Windows.Forms.TextBox CodeTextBox;
+    }
+}
---
+++ scyntaxProject 20-03-2016/scyntaxProject/syntaxPad.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace scyntaxProject
+{
+    public partial class syntaxPad : Form
+    {
+        char[] breakers;
+        string[] keywords_ValuePart;
+        string[] keywords_ClassPart;
+        int line_no;
+
+        public syntaxPad()
+        {
+            InitializeComponent();
+        }
+
+        private void syntaxPad_Load(object sender, EventArgs e)
+        {
+            //Deleted '+','-' and '.' to check the RE of float_constant. We should build some logic for it.
+            breakers = new char[] { '*', '/', '%',';','=', '&', '|', '!', '<', '>', ':', ',', '?', '(', ')', '{', '}', '[', ']',' ','\r','\n',';'};
+
+            // load the value part and class part of keywords in keyword_Array
+            int i = 0;
+            string keywordFile = @"keywords List.txt";
+            
+            string[] lines;
+
+            lines = File.ReadAllLines(keywordFile);
+            keywords_ValuePart = new string[lines.Length];
+            keywords_ClassPart = new string[lines.Length];
+
+            while (i < lines.Length)
+            {
+                int index = lines[i].IndexOf(",");
+                keywords_ValuePart[i] = lines[i].Substring(0, index);
+                keywords_ClassPart[i] = lines[i].Substring(index + 1);
+                //Console.WriteLine("{0} , {1}", keywords_ValuePart[i], keywords_ClassPart[i]); //Done! value part and class part of keywords
+                i++;
+                
+            }
+
+            File.WriteAllText(@"TokensFile.txt", String.Empty);
+        }
+
+        private void CompileButton_Click(object sender, EventArgs e)
+        {
+            line_no = 1;
+            string code, tempStr=null, viewTokens,remainCode;
+            string tokenFile = @"TokensFile.txt";
+            char[] codeCharArr;
+            char hold= '0',holdBreaker='0';
+            int count = 0,index;
+
+            code = CodeTextBox.Text.ToString();
+            codeCharArr = new char[code.Length];
+            codeCharArr = code.ToCharArray();
+
+                for (int i = 0; i < code.Length; i++)
+                {
+                    for (int j = 0; j < breakers.Length; j++)
+                    {
+                        if (codeCharArr[i] != breakers[j])
+                        {
+                            hold = codeCharArr[i];
+                        }
+                        else if (codeCharArr[i] == breakers[j])
+                        {
+                            holdBreaker = codeCharArr[i];
+                            
+                            break;
+                        }
+                    }
+                    if (codeCharArr[i] == holdBreaker)
+                    {
+                        if (holdBreaker == '\n')
+                        {
+                            Console.WriteLine("Line no= " + line_no);
+                            line_no++;
+                        }
+                        index = code.IndexOf(holdBreaker);
+
+                        remainCode = code.Substring(index + 1);
+                        code = remainCode;
+                        codeCharArr = null;
+                        codeCharArr = code.ToCharArray();
+                        if (tempStr != null && KeywordCheck(tempStr) != true)
+                        {
+                            if (IdentifierCheck(tempStr) != true)
+                                if (IntConst(tempStr) != true)
+                                    if (FltConst(tempStr) != true)
+                                        if (CharConst(tempStr) != true)
+                                                StrConst(tempStr);
+                        }
+                        tempStr = null;
+                        i = -1;
+                    }
+                    else
+                    {
+                        tempStr += hold;
+                        count++;
+                    }
+
+                }
+            Console.WriteLine("Line no= "+line_no);
+            //for (int i = 0; i < code.Length; i++)
+            //{
+            //    FileStream fs = new FileStream(@"TokensFile.txt", FileMode.Append, FileAccess.Write);
+            //    StreamWriter Sw = new StreamWriter(fs);
+            //    {
+            //        Sw.WriteLine(codeCharArr[i]);
+            //    }
+            //    Sw.Close();
+            //    fs.Close();
+            //}
+
+            //viewTokens = File.ReadAllText(@"TokensFile.txt");
+            //TokenTextBox.Text = viewTokens;
+        }
+        public bool KeywordCheck(string saveStr)
+        {
+            for (int i = 0; i < keywords_ValuePart.Length; i++)
+            {
+
+                if (saveStr == keywords_ValuePart[i])
+                {
+                    Console.WriteLine(keywords_ValuePart[i]);
+                    Console.WriteLine(keywords_ClassPart[i]);
+                    return true;
+                }
+                    
+            }
+            return false;
+        }
+        public bool IdentifierCheck(string CheckID) {
+
+          Match mc = Regex.Match(CheckID, @"^([a-zA-Z]|_)(\w*[a-zA-Z0-9])*$");
+          if (mc.Success)
+          {
+
+              Console.WriteLine(mc.Value+"\nID");
+              return true;
+          }
+          else
+              return false;
+        }
+        public bool IntConst(string IntegerConstant)
+        {
+
+            Match mc1 = Regex.Match(IntegerConstant, @"^(\+|\-|\s*)\d+$");
+            if (mc1.Success)
+            {
+
+                Console.WriteLine(mc1.Value + "\nInt_Constant");
+                return true;
+            }
+            else
+                return false;   
+        }
+        public bool FltConst(string FloatConstant)
+        {
+
+            Match mc1 = Regex.Match(FloatConstant, @"^(\+|\-|\s*)\d*\.\d+$");
+            if (mc1.Success)
+            {
+
+                Console.WriteLine(mc1.Value + "\nFloat_Constant");
+                return true;
+            }
+            else
+                return false;
+        }
+        public bool CharConst(string CharacterConstant)
+        {
+
+            Match mc1 = Regex.Match(CharacterConstant, @"^\^(\w|\\0|\\n|\\t|\\^|\\)\^$");
+            if (mc1.Success)
+            {
+
+                Console.WriteLine(mc1.Value + "\nChar_Constant");
+                return true;
+            }
+            else
+                return false;
+        }
+        public bool StrConst(string StringConstant)
+        {
+            //Cannot take space between words! Will solve it later.
+            Match mc1 = Regex.Match(StringConstant, @"^\^\^(\w*|\s*|\\n|\\t|\\^)*\^\^$");
+            if (mc1.Success)
+            {
+
+                Console.WriteLine(mc1.Value + "\nString_Constant");
+                return true;
+            }
+            else
+                return false;
+        }
+     
+    }
+}
